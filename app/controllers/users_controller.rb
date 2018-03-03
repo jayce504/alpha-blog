0b659094ca28f4176725fbae189999e1590256c2
Jayce504 <shudson5523@gmail.com>
2018-03-03 13:36:57 +0000

    Added admin requirement to create categories and fixed session id issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = nill
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
       redirect_to user_path(@user)
     else
